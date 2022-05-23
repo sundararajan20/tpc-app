0bade884514045c9bf88ad037cd215692db6b4a7
Sundararajan Renganathan <rsundar@stanford.edu>
2022-05-23 15:53:23 -0400

    Fix slices

--- src/main/java/org/onosproject/tpc/TPCComponent.java
+++ src/main/java/org/onosproject/tpc/TPCComponent.java
@@ -252,7 +252,7 @@ public class TPCComponent implements TPCService {
             // Add rate 2
             bands.add(DefaultBand.builder()
                     .ofType(Band.Type.MARK_RED)
-                    .withRate(sliceQoSEntry.getPir() / 8).burstSize(30000)
+                    .withRate(sliceQoSEntry.getPir() / 8).burstSize(60000)
                     .build());
 
             meterRequest.withBands(bands);
